c4e2d497d7e1cd92c1a57830ba2c1d548d29ee06
andrii29 <andriikrymus@gmail.com>
2018-03-11 09:48:45 +0200

    update database credentials

--- test1/settings.py
+++ test1/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', 
         'NAME': 'test1_DB',
-        'USER': 'test1',
+        'USER': 'andriik',
         'PASSWORD': 'test1_379',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
